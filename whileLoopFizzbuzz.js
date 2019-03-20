7f3220af954b22adcad2d24565ded4ec6207f4ef
Tisha Shurley <tishashurley@hotmail.com>
2019-03-20 14:17:24 -0700

    Create whileLoopFizzbuzz.js

---
+++ whileLoopFizzbuzz.js
@@ -0,0 +1,26 @@
+/*
+ * Programming: JuliaJames (Fizzbuzz)
+ * Write a while loop that:
+ * 
+ * Loop through the numbers 1 to 20
+ * If the number is divisible by 3, print "Julia"
+ * If the number is divisible by 5, print "James"
+ * If the number is divisible by 3 and 5, print "JuliaJames"
+ * If the number is not divisible by 3 or 5, print the number
+ */
+
+var x = 1; // when to start
+
+while (x <= 20) { // when to stop
+	if (x%3===0 && x%5===0){
+	console.log("JuliaJames");
+	} else if (x%3===0){
+	console.log("Julia");
+	} else if (x%5===0) {
+	console.log("James");
+	} else {
+	console.log(x);
+	}
+
+  x = x + 1; // how to get to the next item
+}
