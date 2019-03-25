d924b6f6241a08a7772d0c49c7ef2e755117e039
Tisha Shurley <tishashurley@hotmail.com>
2019-03-25 15:58:10 -0700

    Create forLoopFactorials.js

---
+++ forLoopFactorials.js
@@ -0,0 +1,12 @@
+/*
+ * Programming: Factorials 
+ Write a for (note: not a function) loop that prints out the factorial of the number 12
+ */
+
+let solution = 1;
+
+for (let x = 1; x < 13; x++){
+    solution *=x;
+}
+
+console.log(solution);
