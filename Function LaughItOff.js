6c31de7bdfeb5a56d513ea19ad94d095d0be78ae
Tisha Shurley <tishashurley@hotmail.com>
2019-05-17 16:09:16 -0700

    Create Function LaughItOff.js

---
+++ Function LaughItOff.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Function: Laugh it Off 
+ * Directions:
+ * Declare a function called laugh() that returns "hahahahahahahahahaha!". Print the value returned from the laugh() function to the console.
+ */
+
+function laugh(phrase){
+    let statement = "hahahahahahahahahaha!";
+    return statement;
+}
+
+console.log(laugh());
