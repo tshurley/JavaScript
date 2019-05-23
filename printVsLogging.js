00fe1f7d2a02504eaa79eaeffe253c78538b0f6d
Tisha Shurley <tishashurley@hotmail.com>
2019-05-23 07:45:45 -0700

    Create printVsLogging.js

---
+++ printVsLogging.js
@@ -0,0 +1,8 @@
+function isThisWorking(input) {
+  console.log("Printing: isThisWorking was called and " + input + " was passed in as an argument.");
+  return "Returning: I am returning this string!";
+}
+
+isThisWorking(3);
+//Prints: "Printing: isThisWorking was called and 3 was passed in as an argument"
+//Returns: "Returning: I am returning this string!"
